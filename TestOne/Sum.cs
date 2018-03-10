fda38930b6806afe18138ab7d84b8b8fc520d32d
sayschj <google.app@qq.com>
2018-03-10 13:12:42 +0800

    TestOne_1

---
+++ TestOne/Sum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*C#实验一
+ * 姓名：陈红鹃
+ * 班级：15计科一班
+ * 日期：2018/3/8
+ */
+
+//1、编程求1+2+3+......+100的和。
+namespace TestOne
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int sum = 0;
+            for (a = 0; a <= 100; a++)
+            {
+                sum += a;
+            }
+            Console.WriteLine("1+2+3+.....+100的和是：" + sum);
+            Console.ReadKey();
+        }
+    }
+}
